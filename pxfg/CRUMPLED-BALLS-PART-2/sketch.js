f507ec40a6f01e0152cfedfd976f1b13481fa896
Kunal10207 <72494442+Kunal10207@users.noreply.github.com>
2021-02-20 12:56:06 +0530

    Add files via upload

---
+++ pxfg/CRUMPLED-BALLS-PART-2/sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, gameState,engine, world,dustbin,paper;
+function setup() {
+  createCanvas(1200, 400);
+  rectMode(CENTER);
+
+  gameState = "start";
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  dustbin = new DustBin(690, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
+}
+
+function draw() {
+  if (gameState === "start") {
+    background("green");
+    textSize(20);
+    fill("blue");
+    text("HELP KUNAL TO THROW THE TRASH INTO THE DUSTBIN press up arrow key to move the ball right.", 50, 200)
+    if (keyCode === UP_ARROW) {
+      gameState = "play"
+    }
+  }
+  if (gameState === "play") {
+    rectMode(CENTER);
+    background(0);
+    dustbin.display();
+    paper.display();
+
+  }
+}
+
+
+function keyPressed(){
+  if (keyCode === UP_ARROW && gameState === "play") {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 14,
+      y: -12
+    });
+  }
+}
